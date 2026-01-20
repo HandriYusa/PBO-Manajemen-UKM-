aaa0742b7b8fa845a2edb8785907533d0989a901
Alit.4rya <alitbaim2@gmail.com>
2026-01-20 09:53:31 +0800

    Update UserController.java

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,1 +1,28 @@
 // UserController.java
+
+package controller;
+
+import model.User;
+
+public class UserController {
+
+    // Simulasi user aktif (bisa dikembangkan ke DB)
+    private User userAktif;
+
+    public void login(int idUser, String nama, String role) {
+        userAktif = new User(idUser, nama, role);
+    }
+
+    public User getUserAktif() {
+        return userAktif;
+    }
+
+    public boolean isLogin() {
+        return userAktif != null;
+    }
+
+    public void logout() {
+        userAktif = null;
+    }
+}
+
