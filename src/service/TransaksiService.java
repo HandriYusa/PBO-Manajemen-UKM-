fea1596dc9762d6fd86146b2240d7ab409a4fc16
Alit.4rya <alitbaim2@gmail.com>
2026-01-20 09:56:51 +0800

    Update TransaksiService.java

--- src/service/TransaksiService.java
+++ src/service/TransaksiService.java
@@ -1,1 +1,16 @@
 // TransaksiService.java
+
+package service;
+
+import model.Transaksi;
+
+public class TransaksiService {
+
+    public void jalankanTransaksi(Transaksi transaksi) {
+        if (transaksi == null) {
+            throw new IllegalArgumentException("Transaksi tidak boleh kosong");
+        }
+        transaksi.proses();
+    }
+}
+
